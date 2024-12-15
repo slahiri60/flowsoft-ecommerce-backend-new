34a4840c625cd6a99b706b27f684ebfc41fe68ca
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2024-12-14 20:01:39 -0800

    Implemented displaying related products in backend

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 import Category from '../models/category.js';
+import Product from '../models/product.js';
 import slugify from 'slugify';
 
 export const create = async (req, res) => {
@@ -66,5 +67,19 @@ export const read = async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(400).json(err.message);
+  }
+};
+
+export const productsByCategory = async (req, res) => {
+  try {
+    const category = await Category.findOne({ slug: req.params.slug });
+    const products = await Product.find({ category }).populate('category');
+
+    res.json({
+      category,
+      products,
+    });
+  } catch (err) {
+    console.log(err);
   }
 };
--- routes/category.js
+++ routes/category.js
@@ -5,12 +5,20 @@ const router = express.Router();
 // middlewares
 import { requireSignin, isAdmin } from '../middlewares/auth.js';
 // controllers
-import { create, update, remove, list, read } from '../controllers/category.js';
+import {
+    create,
+    update,
+    remove,
+    list,
+    read,
+    productsByCategory,
+  } from '../controllers/category.js';
 
 router.post('/category', requireSignin, isAdmin, create);
 router.put('/category/:categoryId', requireSignin, isAdmin, update);
 router.delete('/category/:categoryId', requireSignin, isAdmin, remove);
 router.get('/categories', list);
 router.get('/category/:slug', read);
+router.get('/products-by-category/:slug', productsByCategory);
 
 export default router;
